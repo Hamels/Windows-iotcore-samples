5987ef852b788b79c24e39a5230c9926b01d2d35
Stefan Hamels <stefan.hamels@gmail.com>
2019-01-08 22:35:59 +0100

    change to servo

--- Samples/HelloBlinky/CS/MainPage.xaml.cs
+++ Samples/HelloBlinky/CS/MainPage.xaml.cs
@@ -1,7 +1,12 @@
 ﻿// Copyright (c) Microsoft. All rights reserved.
 
 using System;
+using System.Diagnostics;
+using System.Threading;
+using System.Threading.Tasks;
 using Windows.Devices.Gpio;
+using Windows.Foundation;
+using Windows.System.Threading;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -17,6 +22,7 @@ namespace Blinky
         private DispatcherTimer timer;
         private SolidColorBrush redBrush = new SolidColorBrush(Windows.UI.Colors.Red);
         private SolidColorBrush grayBrush = new SolidColorBrush(Windows.UI.Colors.LightGray);
+        private IAsyncAction workItemThread;
 
         public MainPage()
         {
@@ -25,12 +31,15 @@ namespace Blinky
             timer = new DispatcherTimer();
             timer.Interval = TimeSpan.FromMilliseconds(500);
             timer.Tick += Timer_Tick;
+            ulong pulseTicks = ((ulong)(Stopwatch.Frequency) / 1000) * 2;
             InitGPIO();
             if (pin != null)
             {
-                timer.Start();
+                //timer.Start();
             }        
         }
+
+
 
         private void InitGPIO()
         {
@@ -53,8 +62,75 @@ namespace Blinky
 
         }
 
-   
 
+        public void PWM_R(int pinNumber)
+        {
+            var stopwatch = Stopwatch.StartNew();
+            
+
+            workItemThread = Windows.System.Threading.ThreadPool.RunAsync(
+                 (source) =>
+                 {
+                     // setup, ensure pins initialized
+                    // ManualResetEvent mre = new ManualResetEvent(false);
+                    // mre.WaitOne(10);
+
+                     ulong pulseTicks = ((ulong)(Stopwatch.Frequency) / 100) * 2;
+                     ulong delta;
+                     var startTime = stopwatch.ElapsedMilliseconds;
+                     while (stopwatch.ElapsedMilliseconds - startTime <= 1)
+                     {
+                         pin.Write(GpioPinValue.High);
+                         ulong starttick = (ulong)(stopwatch.ElapsedTicks);
+                         while (true)
+                         {
+                             delta = (ulong)(stopwatch.ElapsedTicks) - starttick;
+                             if (delta > pulseTicks) break;
+                         }
+                         pin.Write(GpioPinValue.Low);
+                         starttick = (ulong)(stopwatch.ElapsedTicks);
+                         while (true)
+                         {
+                             delta = (ulong)(stopwatch.ElapsedTicks) - starttick;
+                             if (delta > pulseTicks * 10) break;
+                         }
+                     }
+                 }, WorkItemPriority.High);
+        }
+
+        public void PWM_L(int pinNumber)
+        {
+            var stopwatch = Stopwatch.StartNew();
+
+            workItemThread = Windows.System.Threading.ThreadPool.RunAsync(
+                 (source) =>
+                 {
+                     // setup, ensure pins initialized
+                     ManualResetEvent mre = new ManualResetEvent(false);
+                     mre.WaitOne(1500);
+
+                     ulong pulseTicks = ((ulong)(Stopwatch.Frequency) / 1000) * 2;
+                     ulong delta;
+                     var startTime = stopwatch.ElapsedMilliseconds;
+                     while (stopwatch.ElapsedMilliseconds - startTime <= 300)
+                     {
+                         pin.Write(GpioPinValue.High);
+                         ulong starttick = (ulong)(stopwatch.ElapsedTicks);
+                         while (true)
+                         {
+                             delta = starttick - (ulong)(stopwatch.ElapsedTicks);
+                             if (delta > pulseTicks) break;
+                         }
+                         pin.Write(GpioPinValue.Low);
+                         starttick = (ulong)(stopwatch.ElapsedTicks);
+                         while (true)
+                         {
+                             delta = (ulong)(stopwatch.ElapsedTicks) - starttick;
+                             if (delta > pulseTicks * 10) break;
+                         }
+                     }
+                 }, WorkItemPriority.High);
+        }
 
 
 
@@ -73,7 +149,20 @@ namespace Blinky
                 LED.Fill = grayBrush;
             }
         }
-             
 
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            PWM_L(LED_PIN);
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                PWM_R(LED_PIN);
+                Task.Delay(6).Wait();
+            }
+           
+        }
     }
 }
